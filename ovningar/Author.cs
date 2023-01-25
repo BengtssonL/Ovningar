89df90d4a2afe9c2931ef72addde9ba61ecc4f58
Lucas Bengtsson <lucas.bengtsson77@gmail.com>
2023-01-25 14:45:18 +0100

    Add project files.

---
+++ ovningar/Author.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ovningar
+{
+    class Author
+    {
+        private string name;
+        private string email;
+        private char gender;
+
+        public Author(string name, string email, char gender)
+        {
+            this.name = name;
+            this.email = email;
+            this.gender = gender;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public char getGender()
+        {
+            return gender;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + name + ", Email: " + email + ", Gender: " + gender;
+        }
+    }
+
+
+
+}
---
+++ ovningar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ovningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of authors you want to create:");
+             int numberOfAuthors = int.Parse(Console.ReadLine());
+
+             Author[] authors = new Author[numberOfAuthors];
+
+             for (int i = 0; i < numberOfAuthors; i++)
+             {
+                 Console.WriteLine("Enter the name of author {0}:", i + 1);
+                 string name = Console.ReadLine();
+                 Console.WriteLine("Enter the email of author {0}:", i + 1);
+                 string email = Console.ReadLine();
+                 Console.WriteLine("Enter the gender of author {0} (M/F):", i + 1);
+                 char gender = char.Parse(Console.ReadLine());
+
+                 authors[i] = new Author(name, email, gender);
+             }
+
+             for (int i = 0; i < numberOfAuthors; i++)
+             {
+                 Console.WriteLine(authors[i].ToString());
+             }
+         }
+            
+    }
+
+}
